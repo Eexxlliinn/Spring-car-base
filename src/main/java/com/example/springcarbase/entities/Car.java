e69762335bf9e618b21e3ff159900a9354b8ff37
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-25 03:23:38 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/Car.java
+++ src/main/java/com/example/springcarbase/entities/Car.java
@@ -1,4 +1,24 @@
 package com.example.springcarbase.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String car_number;
+
+    private String model;
+
+    private String capacity;
+
+
 }

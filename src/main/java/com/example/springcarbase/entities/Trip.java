5c77b31403a0cf4c73c795c01b0aca4050ba8a4b
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-11 00:42:56 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/Trip.java
+++ src/main/java/com/example/springcarbase/entities/Trip.java
@@ -1,4 +1,23 @@
 package com.example.springcarbase.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date start_date;
+
+    private Date end_date;
+
+    private String status;
+
+    @OneToOne
+    private Driver driver_id;
 }

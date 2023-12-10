078d6422928dffd5e910ba0edef040150ed36b17
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-11 00:42:50 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/Request.java
+++ src/main/java/com/example/springcarbase/entities/Request.java
@@ -1,4 +1,30 @@
 package com.example.springcarbase.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
 public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+
+    private Date request_date;
+
+    private String distance;
+
+    private String required_capacity;
+
+    private String required_type;
+
+    private String status;
 }

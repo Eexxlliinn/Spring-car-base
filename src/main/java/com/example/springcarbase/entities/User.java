e08456ee712a22e609edf5f5dc00a238d0c40d2a
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-11 00:43:05 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/User.java
+++ src/main/java/com/example/springcarbase/entities/User.java
@@ -1,4 +1,63 @@
 package com.example.springcarbase.entities;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Data
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@RequiredArgsConstructor
+public class User implements UserDetails {
+
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private final String login;
+
+    private final String password;
+
+    @ManyToOne
+    private Role role_id;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

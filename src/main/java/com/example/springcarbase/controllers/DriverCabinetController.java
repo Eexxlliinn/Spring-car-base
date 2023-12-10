3e50bad5a2f83d12527f82de66fcc0599be15edf
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-11 00:42:27 +0300

    commit

--- src/main/java/com/example/springcarbase/controllers/DriverCabinetController.java
+++ src/main/java/com/example/springcarbase/controllers/DriverCabinetController.java
@@ -3,6 +3,7 @@ package com.example.springcarbase.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -15,19 +16,5 @@ public class DriverCabinetController {
         return "driver_cabinet";
     }
 
-    @GetMapping("/request")
-    public String request() {
-        return "driver_request_info";
-    }
-
-    @GetMapping("/set-info")
-    public String setInfo() {
-        return "driver_set_info";
-    }
-
-    @GetMapping("/show-info")
-    public String showInfo() {
-        return "driver_show_info";
-    }
 
 }

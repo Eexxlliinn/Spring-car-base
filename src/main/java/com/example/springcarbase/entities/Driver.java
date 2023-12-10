215e557a9d5e6d88b82e5ef2571d227778488d97
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-10 23:31:37 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/Driver.java
+++ src/main/java/com/example/springcarbase/entities/Driver.java
@@ -1,4 +1,32 @@
 package com.example.springcarbase.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String full_name;
+
+    private String age;
+
+    private String license_number;
+
+    private String category;
+
+    private String phone_number;
+
+    @OneToOne
+    private Car car_id;
+
+    @OneToOne
+    private Request request_id;
+
+    @OneToOne
+    private User user_id;
 }

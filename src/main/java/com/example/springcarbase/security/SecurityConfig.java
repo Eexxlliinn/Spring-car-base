9cd39f6c677bd3ef59013043344bdc80edf37b4e
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-13 12:41:45 +0300

    commit

---
+++ src/main/java/com/example/springcarbase/security/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.example.springcarbase.security;
+
+import com.example.springcarbase.entities.User;
+import com.example.springcarbase.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return login -> {
+            User user = userRepo.findByLogin(login);
+            if (user != null) {
+                return user;
+            } else {
+                throw new UsernameNotFoundException("User '" + login + "' not found");
+            }
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(mvc.pattern("/"), mvc.pattern("/**")).permitAll()
+                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers(mvc.pattern("/design"), mvc.pattern("/orders")).hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(csrfTokenRepository())
+                        .ignoringRequestMatchers(toH2Console())  // disable CSRF for H2
+                )
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                        .defaultSuccessUrl("/cabinet")
+                )
+                .headers(headers ->
+                        headers
+                                .frameOptions(frameOptions -> frameOptions.disable())
+                )
+                .logout(logout ->
+                        logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/")
+                                .permitAll()
+                );
+        return http.build();
+    }
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
+    }
+}

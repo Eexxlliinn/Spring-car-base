29665a6e97921c276148c90a3a7812068878d8da
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-13 12:38:30 +0300

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,9 @@ delete from Drivers cascade;
 delete from Requests cascade;
 delete from Trips cascade;
 delete from Users cascade;
-delete from Roles cascade;
+delete from Role cascade;
+insert into Role(id, role_name) values ('1', 'USER');
+insert into Role(id, role_name) values ('2', 'ADMIN');
+
+drop sequence USERS_SEQ if exists;
+CREATE SEQUENCE USERS_SEQ START WITH 1 INCREMENT BY 50;

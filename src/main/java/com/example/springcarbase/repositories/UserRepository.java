8fd684678cb47b82ead3408d8d9a6ee4e178d146
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-13 12:42:09 +0300

    commit

---
+++ src/main/java/com/example/springcarbase/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.springcarbase.repositories;
+
+import com.example.springcarbase.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByLogin(String login);
+}

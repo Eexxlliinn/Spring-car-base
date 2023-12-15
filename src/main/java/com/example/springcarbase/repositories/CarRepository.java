4f2a7d2f1eb3ebc8ca9eeea784048de5ebb1b1c8
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-15 21:05:34 +0300

    commit

--- src/main/java/com/example/springcarbase/repositories/CarRepository.java
+++ src/main/java/com/example/springcarbase/repositories/CarRepository.java
@@ -4,6 +4,9 @@ import com.example.springcarbase.entities.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+    List<Car> findByStatus(String status);
 }

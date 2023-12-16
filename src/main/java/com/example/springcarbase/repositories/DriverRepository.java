678bc8afb9c3b611d098a8ed0902f8408abff94d
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-16 17:20:27 +0300

    commit

--- src/main/java/com/example/springcarbase/repositories/DriverRepository.java
+++ src/main/java/com/example/springcarbase/repositories/DriverRepository.java
@@ -1,9 +1,13 @@
 package com.example.springcarbase.repositories;
 
+import com.example.springcarbase.entities.Car;
 import com.example.springcarbase.entities.Driver;
+import com.example.springcarbase.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends CrudRepository<Driver, Long> {
+
+    Driver findByUser(User user);
 }

e42a68643fd54d8c4b232456721fa5bcb5910a32
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-16 17:25:57 +0300

    commit

--- src/main/java/com/example/springcarbase/repositories/UserRepository.java
+++ src/main/java/com/example/springcarbase/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.springcarbase.repositories;
 
+import com.example.springcarbase.entities.Role;
 import com.example.springcarbase.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByLogin(String login);
+    User findByUsername(String username);
+
+    boolean existsByUsernameAndRole(String username, Role role);
 }

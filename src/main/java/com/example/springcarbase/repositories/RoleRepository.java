34de5ca261bb8dca7eaa4e4a48dce375cc4f5fa0
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-16 17:24:34 +0300

    commit

---
+++ src/main/java/com/example/springcarbase/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.springcarbase.repositories;
+
+import com.example.springcarbase.entities.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findByRoleName(String roleName);
+}

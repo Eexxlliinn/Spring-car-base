d3620dc481c8e3054ef4bbbd892278420ef10db0
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-18 00:01:08 +0300

    commit

--- src/main/java/com/example/springcarbase/entities/Request.java
+++ src/main/java/com/example/springcarbase/entities/Request.java
@@ -1,30 +1,41 @@
 package com.example.springcarbase.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 @Entity
+@Table(name = "Requests")
+@NoArgsConstructor
 public class Request {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
 
-    private Date request_date;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date requestDate;
 
     private String distance;
 
-    private String required_capacity;
+    private String requiredCapacity;
 
-    private String required_type;
+    private String requiredType;
 
     private String status;
+
+    public Request(String description, Date requestDate, String distance, String requiredCapacity, String requiredType, String status) {
+        this.description = description;
+        this.requestDate = requestDate;
+        this.distance = distance;
+        this.requiredCapacity = requiredCapacity;
+        this.requiredType = requiredType;
+        this.status = status;
+    }
 }
